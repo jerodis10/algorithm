3fe9830d28c99a38d4ebfb65f91d007ba41bb83b
jerodis10 <jerodis10@naver.com>
2022-11-16 23:22:48 +0900

    [baekjoon] Shortest path - review (BJ_1753)

--- ex.java
+++ ex.java
@@ -1,4 +1,6 @@
+//import java.util.ArrayList;
 //import java.util.LinkedList;
+//import java.util.PriorityQueue;
 //import java.util.Queue;
 //
 //import org.junit.Assert;
@@ -6,36 +8,69 @@
 //
 //public class ex {
 //
-//	int result;
-//    boolean[] visited;
+//    int number = 6;
+//    int max = Integer.MAX_VALUE;
+//    ArrayList<ArrayList<int[]>> edge = new ArrayList<>();
+//    int[] vertext = new int[7];
 //
-//	public String solution(String s1, String s2) {
-//		String answer = "";
+//    void dijkstra() {
+//        vertext[start] = 0;
+//        PriorityQueue<int[]> pq = new PriorityQueue<>();
+//        pq.add(new int[]{start, 0});
 //
-//		char[] charArr1 = s1.toCharArray();
-//		char[] charArr2 = s2.toCharArray();
+//        while (!pq.isEmpty()) {
+//            int current = pq.peek()[0];
+//            int distance = pq.peek()[1];
+//            pq.poll();
 //
-//		int index = 0;
-//		for(int i = charArr1.length; i >= 0; i--) {
-//			if(charArr1[i] == charArr2[index]) {
-//				index++;
-//			} else {
-//				index--;
-//				break;
-//			}
-//		}
+//            if(vertex[current] < distance) continue;
 //
-//		answer += s1.subString(0, index);
-//		answer += s2.subString(index, s2.length());
+//            for (int i = 0; i < edge.get(current).size(); i++) {
+//                int next = edge.get(current).get(i)[0];
+//                int nextDistance = edge.get(current).get(i)[1];
+//                if (nextDistance < vertex[next]) {
+//                    vertex[next] = nextDistance;
+//                    pq.add()
+//                }
 //
-//		return answer;
-//	}
+//            }
+//        }
+//    }
 //
-//	@Test
-//	public void testCase() {
-//		Assert.assertEquals("ABCxyZA", solution("xyZA", "ABCxy"));
-////		Assert.assertEquals(1, solution(new int[][] {{1,1,0}, {1,1,1}, {0,1,1}}));
-//	}
+//
+//
+//	int number = 6;
+//    int max = Integer.MAX_VALUE;
+//    ArrayList<ArrayList<int[]>> edge = new ArrayList<>();
+//    int[] vertex = new int[7];
+//
+//    void dijkstra(int start) {
+//        vertex[start] = 0;
+//        PriorityQueue<int[]> pq = new PriorityQueue<>();
+//        pq.add(new int[]{start, 0});
+//
+//        while (!pq.isEmpty()) {
+//            int current = pq.peek()[0];
+//            int distance = pq.peek()[1];
+//            pq.poll();
+//
+//            if(vertex[current] < distance) continue;
+//            for (int i = 0; i < edge.get(current).size(); i++) {
+//                int next = edge.get(current).get(i)[0];
+//                int nextDistance = distance + edge.get(current).get(i)[1];
+//                if (nextDistance < vertex[next]) {
+//                    vertex[next] = nextDistance;
+//                    pq.add(new int[]{next, nextDistance});
+//                }
+//            }
+//        }
+//    }
+//
+////	@Test
+////	public void testCase() {
+////		Assert.assertEquals("ABCxyZA", solution("xyZA", "ABCxy"));
+//////		Assert.assertEquals(1, solution(new int[][] {{1,1,0}, {1,1,1}, {0,1,1}}));
+////	}
 //
 //
 //}
