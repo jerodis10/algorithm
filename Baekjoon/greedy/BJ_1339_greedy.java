38b91f492e586f53010d1e16d266acc623f471d8
jerodis10 <jerodis10@naver.com>
2022-12-16 17:52:23 +0900

    [Baekjoon>greedy] BJ_1339 - 실패

--- Baekjoon/greedy/BJ_1339_greedy.java
+++ Baekjoon/greedy/BJ_1339_greedy.java
@@ -3,7 +3,7 @@ package Baekjoon.greedy;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class BJ_1339_greedy {
 	
@@ -11,35 +11,54 @@ public class BJ_1339_greedy {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		long[] num = new long[n];
+		String[] strArr = new String[n];
 		for (int i = 0; i < n; i++) {
-			num[i] = Integer.parseInt(br.readLine());
+			strArr[i] = br.readLine();
 		}
 
-		System.out.println(solution(n, num));
+		System.out.println(solution(n, strArr));
 	}
 
-	public static long solution(int n, long[] num) {
-		if(n == 1) return 0;
-		if(n == 2) return num[0] + num[1];
+	public static int solution(int n, String[] strArr) {
+		int sum = 0;
+		Map<String, Integer> map = new HashMap<>();
+		Map<String, Integer> strPos = new HashMap<>();
+		boolean[] visited = new boolean[10];
 
-		long sum = 0;
-		PriorityQueue<Long> pq = new PriorityQueue<>();
-		for (long i : num) {
-			pq.add(i);
+		for (String str : strArr) {
+			strPos.put(str, 0);
 		}
-		long count = 0;
-		long temp = 0;
-		while (!pq.isEmpty()) {
-			if (count == 0) {
-				temp += pq.poll();
+		int count = 0;
+		while(count < n) {
+			String str = null;
+			int curPos = -1;
+			int len = -1;
+			for (String key : strPos.keySet()) {
+				if (key.substring(strPos.get(key)).length() > len) {
+					curPos = strPos.get(key);
+					str = key;
+					len = key.substring(strPos.get(key)).length();
+				}
+			}
+			if (str.length() == curPos) {
 				count++;
+				continue;
+			}
+
+			String s1 = str.substring(curPos, curPos + 1);
+			if (!map.containsKey(s1)) {
+				for (int i = 9; i >= 0; i--) {
+					if (!visited[i]) {
+						map.put(s1, i);
+						visited[i] = true;
+						sum += i * Math.pow(10, str.length() - curPos - 1);
+						strPos.put(str, curPos + 1);
+						break;
+					}
+				}
 			} else {
-				temp += pq.poll();
-				sum += temp;
-				pq.add(temp);
-				temp = 0;
-				count = 0;
+				sum += map.get(s1) * Math.pow(10, str.length() - curPos - 1);
+				strPos.put(str, curPos + 1);
 			}
 		}
 
