48e2ea4b981daaf2d8781cae104f48d6d21f9a68
jerodis10 <jerodis10@naver.com>
2022-12-16 15:43:56 +0900

    [Baekjoon>greedy] 문제 추가

---
+++ Baekjoon/greedy/BJ_1715_greedy.java
@@ -0,0 +1,49 @@
+package Baekjoon.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class BJ_1715_greedy {
+	
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		long[] num = new long[n];
+		for (int i = 0; i < n; i++) {
+			num[i] = Integer.parseInt(br.readLine());
+		}
+
+		System.out.println(solution(n, num));
+	}
+
+	public static long solution(int n, long[] num) {
+		if(n == 1) return 0;
+		if(n == 2) return num[0] + num[1];
+
+		long sum = 0;
+		PriorityQueue<Long> pq = new PriorityQueue<>();
+		for (long i : num) {
+			pq.add(i);
+		}
+		long count = 0;
+		long temp = 0;
+		while (!pq.isEmpty()) {
+			if (count == 0) {
+				temp += pq.poll();
+				count++;
+			} else {
+				temp += pq.poll();
+				sum += temp;
+				pq.add(temp);
+				temp = 0;
+				count = 0;
+			}
+		}
+
+		return sum;
+	}
+	
+}
--- Baekjoon/greedy/BJ_1946_greedy.java
+++ Baekjoon/greedy/BJ_1946_greedy.java
@@ -3,7 +3,7 @@ package Baekjoon.greedy;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class BJ_1946_greedy {
 	
@@ -15,8 +15,8 @@ public class BJ_1946_greedy {
 		while(t-- > 0) {
 			int n = Integer.parseInt(br.readLine());
 			int[][] person = new int[n][2];
-			st = new StringTokenizer(br.readLine());
 			for (int i = 0; i < n; i++) {
+				st = new StringTokenizer(br.readLine());
 				person[i][0] = Integer.parseInt(st.nextToken());
 				person[i][1] = Integer.parseInt(st.nextToken());
 			}
@@ -26,7 +26,26 @@ public class BJ_1946_greedy {
 	}
 
 	public static int solution(int n, int[][] person) {
+		Stack<int[]> stack = new Stack<>();
+		Arrays.sort(person, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				if (o1[0] == o2[0]) {
+					return o1[1] - o2[1];
+				}
+				return o1[0] - o2[0];
+			}
+		});
 
+		for (int[] p : person) {
+			if (stack.isEmpty()) {
+				stack.add(p);
+			} else if(p[0] < stack.peek()[0] || p[1] < stack.peek()[1]) {
+				stack.add(p);
+			}
+		}
+
+		return stack.size();
 	}
 	
 }
