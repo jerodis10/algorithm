4b6fa437b90e14fb3ef9c04beb99b55272abefe3
jerodis10 <jerodis10@naver.com>
2022-12-20 21:24:57 +0900

    [Baekjoon>greedy] BJ_1700 - 실패

--- Baekjoon/greedy/BJ_1700_greedy.java
+++ Baekjoon/greedy/BJ_1700_greedy.java
@@ -1,12 +1,12 @@
 package Baekjoon.greedy;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class BJ_1700_greedy {
 	
@@ -16,7 +16,7 @@ public class BJ_1700_greedy {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());
-		int[] numArr = new int[n];
+		int[] numArr = new int[k];
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < k; i++) {
 			numArr[i] = Integer.parseInt(st.nextToken());
@@ -26,52 +26,60 @@ public class BJ_1700_greedy {
 	}
 
 	public static int solution(int n, int k, int[] numArr) {
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int i = 0; i < k; i++) {
+			map.put(numArr[i], map.getOrDefault(numArr[i], 0) + 1);
+		}
+
 		Set<Integer> set = new HashSet<>();
 		int count = 0;
+		int ret = 0;
 		for (int i = 0; i < k; i++) {
 			if (i == 0) {
 				set.add(numArr[i]);
+				map.put(numArr[i], map.get(numArr[i]) - 1);
 				count++;
 			} else {
-				if (!set.contains(numArr[i]) && count < n) {
-					set.add(numArr[i]);
+				if(!set.contains(numArr[i])) {
+					if (count < n) {
+						set.add(numArr[i]);
+						map.put(numArr[i], map.get(numArr[i]) - 1);
+						count++;
+					} else {
+						int minKey = 0;
+						int min = Integer.MAX_VALUE;
+						for (Integer key : set) {
+							if (min > map.get(key)) {
+								min = map.get(key);
+								minKey = key;
+							}
+						}
+						set.remove(minKey);
+						map.put(minKey, map.get(minKey) - 1);
+						set.add(numArr[i]);
+						map.put(numArr[i], map.get(numArr[i]) - 1);
+						count++;
+						ret++;
+					}
+				} else {
+					map.put(numArr[i], map.get(numArr[i]) - 1);
 					count++;
-				}
-				if (!set.contains(numArr[i]) && count >= n) {
-
-				}
-				if(set.contains(numArr[i])){
-
 				}
 			}
 		}
 
-
-		return 0;
+		return ret;
 	}
 
-//	@Test
-//	public void testCase() {
-//		Assertions.assertThat(solution(
-//				2
-//				,1
-//				,new int[][]{{5,10}, {100,100}}
-//				,new Integer[]{11}
-//		)).isEqualTo(
-//				10
-//		);
-//	}
-//
-//	@Test
-//	public void testCase2() {
-//		Assertions.assertThat(solution(
-//				3
-//				,2
-//				,new int[][]{{1,65}, {5,23}, {2,99}}
-//				,new Integer[]{10,2}
-//		)).isEqualTo(
-//				164
-//		);
-//	}
-	
+	@Test
+	public void testCase() {
+		Assertions.assertThat(solution(
+				2
+				,7
+				,new int[]{2,3,2,3,1,2,7}
+		)).isEqualTo(
+				2
+		);
+	}
+
 }
