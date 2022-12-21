097f3237a128f83cf2c16fdf8761e28fc07a5719
jerodis10 <jerodis10@naver.com>
2022-12-22 07:39:20 +0900

    [Baekjoon>greedy] BJ_12904 - 진행

---
+++ Baekjoon/greedy/BJ_12904_greedy.java
@@ -0,0 +1,62 @@
+package Baekjoon.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BJ_12904_greedy {
+	
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] nums = new int[n];
+		for (int i = 0; i < n; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+
+		System.out.println(solution(n, nums));
+	}
+
+	public static int solution(int n, int[] nums) {
+		int sum = 0;
+		Arrays.sort(nums);
+		for (int num : nums) {
+			if (sum + 1 < num) {
+				break;
+			}
+
+			sum += num;
+		}
+
+		return sum + 1;
+	}
+
+//	@Test
+//	public void testCase() {
+//		Assertions.assertThat(solution(
+//				2
+//				,1
+//				,new int[][]{{5,10}, {100,100}}
+//				,new Integer[]{11}
+//		)).isEqualTo(
+//				10
+//		);
+//	}
+//
+//	@Test
+//	public void testCase2() {
+//		Assertions.assertThat(solution(
+//				3
+//				,2
+//				,new int[][]{{1,65}, {5,23}, {2,99}}
+//				,new Integer[]{10,2}
+//		)).isEqualTo(
+//				164
+//		);
+//	}
+	
+}
