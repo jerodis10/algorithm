3e585f8b7815dcf9a9bbe967abf7033dc8469a42
jerodis10 <jerodis10@naver.com>
2023-01-18 20:56:06 +0900

    [Baekjoon>greedy] 2457 - 성공

--- Baekjoon/greedy/BJ_2457_greedy.java
+++ Baekjoon/greedy/BJ_2457_greedy.java
@@ -29,50 +29,70 @@ public class BJ_2457_greedy {
 	}
 
 	public static int solution(int n, int[][] flower) {
-		int[] month = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-//		int[][] visited = new int[12][31];
-		boolean[] visited = new boolean[12];
-
-		Arrays.sort(flower, (int[] o1, int[] o2) -> o1[0] == o2[0] ? o2[1] - o1[1] : o1[0] - o2[0]);
-		if(flower[0][0] != 1 || flower[0][1] != 1) return 0;
-
-		int beforeMon = 0;
-		int beforeDay = 0;
+		int[][] flowers = new int[n][2];
 		for (int i = 0; i < n; i++) {
-			if(beforeMon != 0 && beforeDay != 0) {
-				if(beforeMon > )
-			}
-			for (int j = flower[i][0]; j < flower[i][2] - flower[i][0]; j++) {
-				visited[j] = true;
-			}
-
-			beforeMon = flower[i][2];
-			beforeDay = flower[i][3];
+			flowers[i][0] = flower[i][0] * 100 + flower[i][1];
+			flowers[i][1] = flower[i][2] * 100 + flower[i][3];
 		}
+
+		Arrays.sort(flowers, (o1, o2) -> o1[0] == o2[0] ? o2[1] - o1[1] : o1[0] - o2[0]);
+
+		int start = 301;
+		int end = 1201;
+		int count = 0;
+		int max = 0;
+		int index = 0;
+
+		while (start < end) {
+			boolean flag = false;
+			for (int i = index; i < n; i++) {
+				if (flowers[i][0] > start) {
+					break;
+				}
+				if (max < flowers[i][1]) {
+					flag = true;
+					max = flowers[i][1];
+					index = i + 1;
+				}
+			}
+
+			if (flag) {
+				start = max;
+				count++;
+			} else {
+				break;
+			}
+		}
+
+		if (max < end) {
+			return 0;
+		}
+
+		return count;
 	}
 
-	@Test
-	public void testCase() {
-		Assertions.assertThat(solution(
-				2
-				,1
-				,new int[][]{{5,10}, {100,100}}
-				,new Integer[]{11}
-		)).isEqualTo(
-				10
-		);
-	}
-
-	@Test
-	public void testCase2() {
-		Assertions.assertThat(solution(
-				3
-				,2
-				,new int[][]{{1,65}, {5,23}, {2,99}}
-				,new Integer[]{10,2}
-		)).isEqualTo(
-				164
-		);
-	}
+//	@Test
+//	public void testCase() {
+//		Assertions.assertThat(solution(
+//				2
+//				,1
+//				,new int[][]{{5,10}, {100,100}}
+//				,new Integer[]{11}
+//		)).isEqualTo(
+//				10
+//		);
+//	}
+//
+//	@Test
+//	public void testCase2() {
+//		Assertions.assertThat(solution(
+//				3
+//				,2
+//				,new int[][]{{1,65}, {5,23}, {2,99}}
+//				,new Integer[]{10,2}
+//		)).isEqualTo(
+//				164
+//		);
+//	}
 	
 }
