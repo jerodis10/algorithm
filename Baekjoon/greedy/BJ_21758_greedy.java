46e2b25ec08b5c6f009d951b31baec72f030c5f4
jerodis10 <jerodis10@naver.com>
2023-01-19 07:35:02 +0900

    [Baekjoon>greedy] 21758 - 성공

--- Baekjoon/greedy/BJ_21758_greedy.java
+++ Baekjoon/greedy/BJ_21758_greedy.java
@@ -25,19 +25,57 @@ public class BJ_21758_greedy {
 		System.out.println(solution(n, num));
 	}
 
-	public static int solution(int n, int[] num) {
-		return 1;
+	public static long solution(int n, int[] num) {
+		long[] rightSum = new long[n];
+		long[] leftSum = new long[n];
+		long sum = 0;
+		long total = 0;
+		long b1 = 0;
+		long b2 = 0;
+		long max = 0;
+		for (int i = 0; i < n; i++) {
+			sum += num[i];
+			rightSum[i] = sum;
+		}
+
+		sum = 0;
+		for (int i = n - 1; i >= 0; i--) {
+			sum += num[i];
+			leftSum[i] = sum;
+		}
+
+		total = rightSum[n - 1];
+
+		for (int i = 1; i <= n - 2; i++) {
+			b1 = total - num[0] - num[i];
+			b2 = total - rightSum[i];
+			max = Math.max(max, b1 + b2);
+		}
+
+		for (int i = n - 2; i >= 1; i--) {
+			b1 = total - num[n - 1] - num[i];
+			b2 = total - leftSum[i];
+			max = Math.max(max, b1 + b2);
+		}
+
+		for (int i = 1; i <= n - 2; i++) {
+			b1 = rightSum[i] - num[0];
+			b2 = leftSum[i] - num[n - 1];
+			max = Math.max(max, b1 + b2);
+		}
+
+		return max;
 	}
 
-	@Test
-	public void testCase() {
-		Assertions.assertThat(solution(
-				2
-				,7
-				,new int[]{2,3,2,3,1,2,7}
-		)).isEqualTo(
-				2
-		);
-	}
+//	@Test
+//	public void testCase() {
+//		Assertions.assertThat(solution(
+//				2
+//				,7
+//				,new int[]{2,3,2,3,1,2,7}
+//		)).isEqualTo(
+//				2
+//		);
+//	}
 
 }
