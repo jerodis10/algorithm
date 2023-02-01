baee0486cfa3e142ad853ce69ce7f741b1d60757
jerodis10 <jerodis10@naver.com>
2023-02-01 19:46:15 +0900

    [Baekjoon>dynamicProgramming] 1915, 2011, 12852 - review

--- Baekjoon/dynamicProgramming/BJ_1915_dynamicProgramming.java
+++ Baekjoon/dynamicProgramming/BJ_1915_dynamicProgramming.java
@@ -31,12 +31,27 @@ public class BJ_1915_dynamicProgramming {
 		System.out.println(solution());
 	}
 
+//	public static int solution() {
+//		int ret = 0;
+//		for (int i = 1; i <= n; i++) {
+//			for (int j = 1; j <= m; j++) {
+//				if (map[i][j] == 1) {
+//					dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
+//					ret = Math.max(ret, dp[i][j]);
+//				}
+//			}
+//		}
+//
+//		return ret * ret;
+//	}
+
 	public static int solution() {
 		int ret = 0;
+
 		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= m; j++) {
 				if (map[i][j] == 1) {
-					dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
+					dp[i][j] = check(i, j);
 					ret = Math.max(ret, dp[i][j]);
 				}
 			}
@@ -44,5 +59,16 @@ public class BJ_1915_dynamicProgramming {
 
 		return ret * ret;
 	}
-	
+
+	static int check(int y, int x) {
+		int check1 = dp[y - 1][x - 1];
+		int check2 = dp[y][x - 1];
+		int check3 = dp[y - 1][x];
+
+		if (check1 == 0 || check2 == 0 || check3 == 0) {
+			return 1;
+		}
+
+		return Math.min(Math.min(check1, check2), check3) + 1;
+	}
 }
---
+++ Baekjoon/dynamicProgramming/BJ_2011_dynamicProgramming.java
@@ -0,0 +1,49 @@
+package Baekjoon.dynamicProgramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class BJ_2011_dynamicProgramming {
+
+	static final int MOD = 1_000_000;
+	
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+
+		System.out.println(solution(str));
+	}
+
+	public static int solution(String str) {
+		int[] dp = new int[str.length()];
+		int len = str.length();
+
+		if (len == 0 || str.charAt(0) == '0') {
+			return 0;
+		}
+
+		dp[0] = 1;
+
+		for (int i = 1; i < len; i++) {
+			if (str.charAt(i) == '0') {
+				if (str.charAt(i - 1) == '1' || str.charAt(i - 1) == '2') {
+					dp[i] = dp[i - 1] % MOD;
+				} else {
+					return 0;
+				}
+			} else {
+				if (str.charAt(i - 1) == '0') {
+					dp[i] = dp[i - 1];
+				} else if (i + 1 < len && str.charAt(i + 1) == '0') {
+
+				}
+			}
+		}
+
+		return 0;
+	}
+
+}
