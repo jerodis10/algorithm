60b03e5dde3088ebd60ac986221cf91af594a6b4
jerodis10 <jerodis10@naver.com>
2023-03-03 19:51:15 +0900

    [Baekjoon>dynamicProgramming] 1463 - review

--- Baekjoon/dynamicProgramming/BJ_1463_dynamicProgramming8.java
+++ Baekjoon/dynamicProgramming/BJ_1463_dynamicProgramming8.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class BJ_1463_dynamicProgramming8 {
-	
+
 	static int[] dp;
 
     public static void main(String[] args) throws NumberFormatException, IOException {
@@ -17,25 +17,33 @@ public class BJ_1463_dynamicProgramming8 {
         System.out.println(recur(n));
     }
 
-     static int recur(int N) {
-        if (dp[N] == 0) {
-            // 6으로 나눠지는 경우
-            if (N % 6 == 0) {
-                dp[N] = Math.min(recur(N - 1), Math.min(recur(N / 3), recur(N / 2))) + 1;
-            }
-            // 3으로만 나눠지는 경우
-            else if (N % 3 == 0) {
-                dp[N] = Math.min(recur(N / 3), recur(N - 1)) + 1;
-            }
-            // 2로만 나눠지는 경우
-            else if (N % 2 == 0) {
-                dp[N] = Math.min(recur(N / 2), recur(N - 1)) + 1;
-            }
-            // 2와 3으로 나누어지지 않는 경우
-            else {
-                dp[N] = recur(N - 1) + 1;
-            }
-        }
-        return dp[N];
+    static int recur(int n) {
+         if(n == 1) return 0;
+         if(dp[n] > 0) return dp[n];
+
+         dp[n] = recur(n - 1) + 1;
+
+         if (n % 2 == 0) {
+             int tmp = recur(n / 2) + 1;
+             dp[n] = (dp[n] > tmp) ? tmp : dp[n];
+         }
+
+         if (n % 3 == 0) {
+             int tmp = recur(n / 3) + 1;
+             dp[n] = (dp[n] > tmp) ? tmp : dp[n];
+         }
+
+         return dp[n];
     }
+
+    // 풀이 2
+//    static int recur(int n) {
+//        for (int i = 2; i <= n; i++) {
+//            dp[i] = dp[i - 1] + 1;
+//            if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+//            if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+//        }
+//
+//        return dp[n];
+//    }
 }
