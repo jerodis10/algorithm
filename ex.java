ed3f0991037540dc6aa187ed79fcb3ec308a3d88
jerodis10 <jerodis10@naver.com>
2023-03-11 12:17:03 +0900

    디렉토리 정리

--- ex.java
+++ ex.java
@@ -5,86 +5,85 @@ import java.util.*;
 
 public class ex {
 
-	public static int solution(int n, int[][] meetings) {
-		int answer = 0;
 
-		Arrays.sort(meetings, new Comparator<int[]>() {
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				return o1[0] - o2[0];
-			}
-		});
+	public static int solution(List<Integer> box) {
+//		Collections.sort(box);
+//		long left = box.get(0);
+		long left = 1;
+		int sum = 0;
+		for(int i : box) sum += i;
+		long right = sum;
+		long ret = 0;
 
-		int count = 0;
-		int time = 1;
-		int index = 0;
-		int roomIndex = 0;
-		int[] rooms = new int[n];
-		int[] result = new int[n];
-		while (count < meetings.length) {
-			for (int i = 0; i < n; i++) {
-				if (rooms[i] > 0) {
-					rooms[i]--;
+		while (left < right) {
+			long mid = (left + right) / 2;
+			long key = 0;
+//			long max = maxNum(box);
+//			if (mid > max) key = mid;
+//			else {
+			List<Integer> temp = new ArrayList<>();
+			for(int i : box) temp.add(i);
+			for (int i = temp.size() - 1; i >= 1; i--) {
+				int num = temp.get(i);
+				if (num > mid) {
+					temp.set(i, (int) mid);
+					temp.set(i - 1, temp.get(i - 1) + (num - (int) mid));
 				}
 			}
-			if (time <= meetings[index][0]) {
-				if(rooms[roomIndex] > 0){
-					if (roomIndex == n - 1) {
-						roomIndex = 0;
-					} else {
-						roomIndex++;
-					}
-					time++;
-					continue;
-				}
-				rooms[roomIndex] = time + meetings[index][1];
-				result[roomIndex]++;
-				if (roomIndex == n - 1) {
-					roomIndex = 0;
-				} else {
-					roomIndex++;
-				}
-				index++;
-				count++;
-				time++;
-			} else {
-				time++;
+			key = maxNum(temp);
+
+			if (key <= mid) {
+				right = mid;
+			} else if(key > mid) {
+				left = mid + 1;
 			}
+//			else {
+//				ret = mid;
+//				break;
+////				return (int) left;
+//			}
 		}
 
-		int max = Integer.MIN_VALUE;
-		for (int i = 0; i < n; i++) {
-			if (max < result[i]) {
-				max = result[i];
-				if (answer > i) {
-					answer = i;
-				}
-			}
-		}
-
-		return answer;
+//		return (int) ret;
+		return (int) left;
 	}
+
+
+	public static int maxNum (List <Integer> list) {
+		int max = Integer.MIN_VALUE;
+		for (int n : list) max = Math.max(max, n);
+		return max;
+	}
+
 
 	@Test
 	public void testCase() {
 		Assertions.assertThat(solution(
-				2, new int[][] {{0,10},{1,5},{2,7},{3,4}}
-		)).isEqualTo(0);
+				Arrays.asList(1,5,7,6)
+		)).isEqualTo(5);
 	}
 
 	@Test
 	public void testCase2() {
 		Assertions.assertThat(solution(
-				3, new int[][] {{1,20},{2,10},{3,5},{4,9},{6,8}}
-		)).isEqualTo(1);
+				Arrays.asList(5,15,19)
+		)).isEqualTo(13);
 	}
 
-//	@Test
-//	public void testCase3() {
-//		Assertions.assertThat(solution(
-//				4,4,6
-//		)).isEqualTo(7);
-//	}
+	@Test
+	public void testCase3() {
+		Assertions.assertThat(solution(
+				Arrays.asList(10,3,5,7)
+		)).isEqualTo(10);
+	}
+
+	@Test
+	public void testCase4() {
+		Assertions.assertThat(solution(
+				Arrays.asList(3,8,11,7)
+		)).isEqualTo(8);
+	}
+
 
 
 }
