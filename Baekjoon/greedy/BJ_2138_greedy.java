c6bdfd301843e4b3c1ea2c8f3b30c57ac93e5d80
jerodis10 <jerodis10@naver.com>
2023-03-28 21:49:51 +0900

    [Baekjoon>greedy] BJ_2138 - 성공

--- Baekjoon/greedy/BJ_2138_greedy.java
+++ Baekjoon/greedy/BJ_2138_greedy.java
@@ -1,8 +1,5 @@
 package Baekjoon.greedy;
 
-import org.assertj.core.api.Assertions;
-import org.junit.Test;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -11,60 +8,70 @@ public class BJ_2138_greedy {
 
 	static int n;
 	static int ret;
+	static int[][] arr;
+	static int[] target;
 	
 	public static void main(String[] args) throws IOException {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine());
-		char[] init = br.readLine().toCharArray();
-		char[] target = br.readLine().toCharArray();
+		arr = new int[2][n];
+		target = new int[n];
+		String input = br.readLine();
+		String input2 = br.readLine();
+		for (int i = 0; i < input.length(); i++) {
+			arr[0][i] = input.charAt(i) - '0';
+			arr[1][i] = input.charAt(i) - '0';
+			target[i] = input2.charAt(i) - '0';
+		}
 
-		System.out.println(solution(n, init, target));
+		System.out.println(solution(n, arr, target));
 	}
 
-	public static int solution(int n2, char[] init, char[] target) {
+	public static int solution(int a, int[][] b, int[] c) {
 		ret = Integer.MAX_VALUE;
-		n = n2;
-		dfs(1, 0, init, target);
-		dfs(1, 1, switchOn(0, init), target);
+		n = a;
+		arr = b;
+		target = c;
+
+		dfs(1, 0, 0);
+		onoff(0, 1);
+		dfs(1, 1, 1);
 
 		return ret == Integer.MAX_VALUE ? -1 : ret;
 	}
 
-	static void dfs(int depth, int count, char[] arr, char[] target) {
+	static void dfs(int depth, int type, int count) {
 		if (depth == n) {
-			if (arr[depth - 1] == target[depth - 1]) {
+			if (arr[type][depth - 1] == target[depth - 1])
 				ret = Math.min(ret, count);
-			}
 			return;
 		}
 
-		if (arr[depth - 1] != target[depth - 1]) {
-			dfs(depth + 1, count + 1, switchOn(depth, arr), target);
+		if (arr[type][depth - 1] != target[depth - 1]) {
+			onoff(depth, type);
+			dfs(depth + 1, type, count + 1);
 		} else {
-			dfs(depth + 1, count, arr, target);
+			dfs(depth + 1, type, count);
 		}
 	}
 
-	static char[] switchOn(int depth, char[] arr) {
+	static void onoff(int depth, int type) {
 		for (int i = depth - 1; i <= depth + 1; i++) {
 			if (i >= 0 && i < n) {
-				arr[i] = arr[i] == '0' ? '1' : '0';
+				arr[type][i] = arr[type][i] == 1 ? 0 : 1;
 			}
 		}
-		return arr;
 	}
 
-	@Test
-	public void testCase() {
-		Assertions.assertThat(solution(
-				3
-				,new char[]{'0','0','0'}
-				,new char[]{'0','1','0'}
-		)).isEqualTo(
-				3
-		);
-	}
-
+//	@Test
+//	public void testCase() {
+//		Assertions.assertThat(solution(
+//				3
+//				,new char[]{'0','0','0'}
+//				,new char[]{'0','1','0'}
+//		)).isEqualTo(
+//				3
+//		);
+//	}
 
 }
