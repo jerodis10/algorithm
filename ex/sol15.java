7b59eb5dd4d5f60a519d15f13281111f5644cbac
jerodis10 <jerodis10@naver.com>
2023-07-07 15:32:38 +0900

    [programmers] 내용 추가

--- ex/sol15.java
+++ ex/sol15.java
@@ -3,20 +3,35 @@ package ex;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 public class sol15 {
 
-	public int solution(int n, int k, int[][] roads) {
-		return 0;
+	public int solution(int[][] targets) {
+		Arrays.sort(targets, ((o1, o2) -> {
+			if(o1[1] == o2[1]) return o1[0] - o2[0];
+			return o1[1] - o2[1];
+		}));
+
+		int cur = targets[0][1] - 1;
+		int count = 1;
+		for (int[] tar : targets) {
+			if (tar[0] > cur) {
+				cur = tar[1] - 1;
+				count++;
+			}
+		}
+
+		return count;
 	}
 
 
 	@Test
 	public void testCase() {
 		Assertions.assertThat(solution(
-				6,17,
-				new int[][]{{5,4,6},{5,2,5},{0,4,2},{2,3,3},{1,2,7},{0,1,3}}
+				new int[][]{{4,5},{4,8},{10,14},{11,13},{5,12},{3,7},{1,4}}
 		)).isEqualTo(
-				new int[]{1,2,3}
+				3
 		);
 	}
 
---
+++ ex/sol16.java
@@ -0,0 +1,61 @@
+package ex;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class sol16 {
+
+	int[] map;
+	int count = 0;
+	int num;
+
+	public int solution(int n) {
+		num = n;
+		map = new int[n];
+
+		dfs(0);
+
+		return count;
+	}
+
+	private void dfs(int depth) {
+		if (depth == num) {
+			count++;
+			return;
+		}
+
+		for (int i = 0; i < num; i++) {
+			map[depth] = i;
+			if (isCheck(depth)) {
+				dfs(depth + 1);
+			}
+		}
+	}
+
+	private boolean isCheck(int col) {
+
+		for (int i = 0; i < col; i++) {
+			if (map[col] == map[i]) {
+				return false;
+			}
+
+			else if (Math.abs(col - i) == Math.abs(map[col] - map[i])) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+
+	@Test
+	public void testCase() {
+		Assertions.assertThat(solution(
+				4
+		)).isEqualTo(
+				2
+		);
+	}
+
+
+}
